1feb66445e5fb104f7fa31c4a9c11c5d184bd878
JinWooShin <jwshin@egi.utah.edu>
2014-12-07 01:32:06 -0700

    panel

--- js/controllers/ToolbarCtrl.js
+++ js/controllers/ToolbarCtrl.js
@@ -2,5 +2,25 @@
  * Created by Jin Woo Shin on 12/6/2014.
  */
 (function() {
+    'use strict';
 
+    define([
+        'angular'
+    ], function(angular) {
+        function init(App) {
+            App.controller('ToolbarCtrl', ['$rootScope',  '$scope', 'attrs', function ($rootScope, $scope, $attrs){
+
+            }]);
+            App.directive('toolbar', function() {
+               return {
+                   restrict: 'EA',
+                   controller: 'ToolbarCtrl',
+                   link: function(scope, element, attrs, ctrl) {
+                       ctrl.init(element);
+                   }
+               }
+            });
+        };
+        return {start: init};
+    })
 }).call(this);
